d70fd680416557f6e23f220f12e78f165f712bf3
Nat Zimmermann <nat@natzim.me>
2015-08-30 21:43:19 +0100

    Planned out basic routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+get('/', ['as' => 'index', 'uses' => 'HomeController@index']);
+
+Route::group(['prefix' => 'user', 'as' => 'users.'], function () {
+    get('{username}', ['as' => 'show', 'uses' => 'UserController@show']);
+});
+
+Route::group(['prefix' => 'sub', 'as' => 'subs.'], function () {
+    get('{name}', ['as' => 'show', 'uses' => 'SubController@show']);
+    Route::group(['prefix' => 'post', 'as' => 'posts.'], function () {
+        get('{id}', ['as' => 'show', 'uses' => 'PostController@show']);
+    });
 });
