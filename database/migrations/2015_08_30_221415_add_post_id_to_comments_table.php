647f6825368577b048f0becca21f024a22229806
Nat Zimmermann <nat@natzim.me>
2015-08-30 23:17:35 +0100

    Added post id to comments table (whoops)

---
+++ database/migrations/2015_08_30_221415_add_post_id_to_comments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPostIdToCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->integer('post_id')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropColumn('post_id');
+        });
+    }
+}
