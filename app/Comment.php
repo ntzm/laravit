079fe977b6cc5d8fa0c2e466115bba1f7aada0bd
Nat Zimmermann <nat@natzim.me>
2015-08-30 21:19:02 +0100

    Added comments model

---
+++ app/Comment.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'comments';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['content'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function post()
+    {
+        return $this->belongsTo('App\Post');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo('App\Comment', 'parent_id');
+    }
+}
