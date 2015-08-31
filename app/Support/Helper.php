4aeb397183358f5b4a448f04d6866162b3ad71af
Nat Zimmermann <nat@natzim.me>
2015-08-31 11:24:16 +0100

    Fixed 404s becoming strange errors if route was not named

--- app/Support/Helper.php
+++ app/Support/Helper.php
@@ -8,6 +8,9 @@ class Helper
 {
     public static function active($route, $active = 'active', $default = '')
     {
+        if (is_null(Route::getCurrentRoute())) {
+            return $default;
+        }
         return Route::getCurrentRoute()->getName() == $route ? $active : $default;
     }
 }
