f6c9e3921561a1343c7631a4396b4b4d92e27f56
Nat Zimmermann <nat@natzim.me>
2015-09-02 14:50:47 +0100

    New comment interface only shown for signed in users

--- resources/views/subs/posts/show.blade.php
+++ resources/views/subs/posts/show.blade.php
@@ -5,28 +5,30 @@
 @section('content')
     @include('partials.post', compact('post'))
     <h2>Comments</h2>
-    <div class="row">
-        <div class="col-lg-6">
-            <form method="post" action="{{ route('subs.posts.comments.store', [$post->sub->name, $post->slug]) }}">
-                {!! csrf_field() !!}
-                <div class="form-group">
-                    <label for="content">New comment</label>
-                    <textarea rows="10" class="form-control" name="content" id="content" data-preview="#content-preview" data-markdown>{{ old('content') }}</textarea>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-        </div>
-        <div class="col-lg-6">
-            <label>Preview</label>
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <a href="{{ route('users.show', Auth::user()->username) }}">{{ Auth::user()->username }}</a>
-                    just now
-                    <p id="content-preview"></p>
+    @if (Auth::check())
+        <div class="row">
+            <div class="col-lg-6">
+                <form method="post" action="{{ route('subs.posts.comments.store', [$post->sub->name, $post->slug]) }}">
+                    {!! csrf_field() !!}
+                    <div class="form-group">
+                        <label for="content">New comment</label>
+                        <textarea rows="10" class="form-control" name="content" id="content" data-preview="#content-preview" data-markdown>{{ old('content') }}</textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+            <div class="col-lg-6">
+                <label>Preview</label>
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <a href="{{ route('users.show', Auth::user()->username) }}">{{ Auth::user()->username }}</a>
+                        just now
+                        <p id="content-preview"></p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <hr>
+        <hr>
+    @endif
     @include('partials.comments', ['comments' => $post->comments])
 @endsection
