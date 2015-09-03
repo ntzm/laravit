37fa5619c0be60f32cc4001f145368e190962e51
Nat Zimmermann <nat@natzim.me>
2015-09-03 10:40:23 +0100

    Moved post score

--- resources/views/partials/post.blade.php
+++ resources/views/partials/post.blade.php
@@ -14,18 +14,20 @@
             by <a href="{{ route('users.show', $post->user->username) }}">{{ $post->user->username }}</a>
             to <a href="{{ route('subs.show', $post->sub->name) }}">/sub/{{ $post->sub->name }}</a>
         </p>
+        {{ $score }} point{{ $score ? '' : 's' }}
         <a href="{{ route('subs.posts.show', [$post->sub->name, $post->slug]) }}">
             {{ $post->comments()->count() }} comment{{ $post->comments()->count() ? '' : 's' }}
         </a>
         @if(Auth::check())
             <hr>
-            <button type="button" class="btn {{ $voteValue == 1 ? 'btn-info' : 'btn-default' }}" data-vote="1">
-                <span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span>
-            </button>
-            {{ $score }}
-            <button type="button" class="btn {{ $voteValue == -1 ? 'btn-info' : 'btn-default' }}" data-vote="-1">
-                <span class="glyphicon glyphicon-thumbs-down" aria-hidden="true"></span>
-            </button>
+            <div class="btn-group" role="group">
+                <button type="button" class="btn {{ $voteValue == 1 ? 'btn-info' : 'btn-default' }}" data-vote="1">
+                    <span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span>
+                </button>
+                <button type="button" class="btn {{ $voteValue == -1 ? 'btn-info' : 'btn-default' }}" data-vote="-1">
+                    <span class="glyphicon glyphicon-thumbs-down" aria-hidden="true"></span>
+                </button>
+            </div>
         @endif
     </div>
 </div>
