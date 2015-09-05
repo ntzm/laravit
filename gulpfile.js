f607ee2b761e130faaae82db53b8df5c88d8b7c2
Nat Zimmermann <nat@natzim.me>
2015-09-05 16:05:16 +0100

    Cleaned up gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,27 @@ var elixir = require('laravel-elixir');
 var assetsDir = './resources/assets/';
 
 elixir(function(mix) {
-    mix.styles([
-        'bower/bootstrap/dist/css/bootstrap.css',
-        'css/main.css'
-    ], 'public/css/style.css', assetsDir);
-    mix.scripts([
-        'bower/jquery/dist/jquery.js',
-        'bower/bootstrap/dist/js/bootstrap.js',
-        'bower/commonmark/dist/commonmark.js',
-        'js/main.js'
-    ], 'public/js/app.js', assetsDir);
-    mix.copy(assetsDir + 'bower/bootstrap/fonts', 'public/fonts');
+    mix
+        .styles(
+            [
+                'bower/bootstrap/dist/css/bootstrap.css',
+                'css/main.css'
+            ],
+            'public/css/style.css',
+            assetsDir
+        )
+        .scripts(
+            [
+                'bower/jquery/dist/jquery.js',
+                'bower/bootstrap/dist/js/bootstrap.js',
+                'bower/commonmark/dist/commonmark.js',
+                'js/main.js'
+            ],
+            'public/js/app.js',
+            assetsDir
+        )
+        .copy(
+            assetsDir + 'bower/bootstrap/fonts',
+            'public/fonts'
+        );
 });
