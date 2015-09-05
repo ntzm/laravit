5c7ae843f8685fd77c3bc257c828b58b08c392e9
Nat Zimmermann <nat@natzim.me>
2015-09-05 16:58:28 +0100

    Updated post

--- resources/views/partials/post.blade.php
+++ resources/views/partials/post.blade.php
@@ -1,35 +1,32 @@
-<div class="panel panel-default" data-sub="{{ $post->sub->name }}" data-slug="{{ $post->slug }}">
-    <div class="panel-heading">
-        <h4>
-            <a href="{{ Helper::isValidUrl($post->content) ? $post->content : route('subs.posts.show', [$post->sub->name, $post->slug]) }}">
-                {{ $post->title }}
-            </a>
-        </h4>
-    </div>
-    <div class="panel-body">
-        @unless(Helper::isValidUrl($post->content))
-            <p>{!! Helper::markdownToHtml($post->content) !!}</p>
-            <hr>
-        @endunless
-        <p>
-            submitted {{ $post->created_at->diffForHumans() }}
-            by <a href="{{ route('users.show', $post->user->username) }}">{{ $post->user->username }}</a>
-            to <a href="{{ route('subs.show', $post->sub->name) }}">/sub/{{ $post->sub->name }}</a>
-        </p>
-        @choice('general.count.points', $score)
-        <a href="{{ route('subs.posts.show', [$post->sub->name, $post->slug]) }}">
-            @choice('general.count.comments', $post->comments()->count())
+<div class="card card-block" data-sub="{{ $post->sub->name }}" data-slug="{{ $post->slug }}">
+    <h4 class="card-title">
+        <a href="{{ Helper::isValidUrl($post->content) ? $post->content : route('subs.posts.show', [$post->sub->name, $post->slug]) }}">
+            {{ $post->title }}
         </a>
-        @if(Auth::check())
-            <hr>
-            <div class="btn-group" role="group">
-                <button type="button" class="btn {{ $voteValue == 1 ? 'btn-info' : 'btn-default' }}" data-vote="1">
-                    <span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span>
-                </button>
-                <button type="button" class="btn {{ $voteValue == -1 ? 'btn-info' : 'btn-default' }}" data-vote="-1">
-                    <span class="glyphicon glyphicon-thumbs-down" aria-hidden="true"></span>
-                </button>
-            </div>
-        @endif
-    </div>
+    </h4>
+    <h6 class="card-subtitle">
+        @choice('general.count.points', $score)
+        <span class="text-muted">{{ $post->created_at->diffForHumans() }}</span>
+    </h6>
+    @unless(Helper::isValidUrl($post->content))
+        <div class="card-text">
+            {!! Helper::markdownToHtml($post->content) !!}
+        </div>
+    @endunless
+    <a class="card-link" href="{{ route('subs.posts.show', [$post->sub->name, $post->slug]) }}">
+        @choice('general.count.comments', $post->comments()->count())
+    </a>
+    <a class="card-link" href="{{ route('subs.show', $post->sub->name) }}">/sub/{{ $post->sub->name }}</a>
+    <a class="card-link" href="{{ route('users.show', $post->user->username) }}">/user/{{ $post->user->username }}</a>
+    @if(Auth::check())
+        <hr>
+        <div class="btn-group" role="group">
+            <button type="button" class="btn btn-{{ $voteValue == 1 ? 'primary' : 'secondary' }}-outline" data-vote="1">
+                Up
+            </button>
+            <button type="button" class="btn btn-{{ $voteValue == -1 ? 'primary' : 'secondary' }}-outline" data-vote="-1">
+                Down
+            </button>
+        </div>
+    @endif
 </div>
