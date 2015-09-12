aba3ce164f7109332b9e35d399c7d9044b697c5e
Nat Zimmermann <nat@natzim.me>
2015-09-12 22:22:20 +0100

    Modify title to always include Laravit string

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title', 'Laravit')</title>
+    <title>@yield('title', 'Laravit') | Laravit</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
