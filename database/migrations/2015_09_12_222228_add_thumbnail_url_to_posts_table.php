04aa4eb12e352ccd55fa619369bb54cc7ec402f6
Nat Zimmermann <nat@natzim.me>
2015-09-12 23:23:52 +0100

    Add thumbnail_url migration

---
+++ database/migrations/2015_09_12_222228_add_thumbnail_url_to_posts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddThumbnailUrlToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('thumbnail_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('thumbnail_url');
+        });
+    }
+}
