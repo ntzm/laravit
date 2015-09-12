9076fb6def23ef0f2b9d588a6270c375e35e440f
Nat Zimmermann <nat@natzim.me>
2015-09-12 20:17:52 +0100

    Move links to right side of navbar

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -4,13 +4,13 @@
     </button>
     <div class="collapse navbar-toggleable-xs" id="navbar">
         <a class="navbar-brand" href="{{ route('index') }}">Laravit</a>
-        <ul class="nav navbar-nav">
+        <ul class="nav navbar-nav pull-right">
             @if(Auth::check())
                 <li class="nav-item dropdown">
                     <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" type="button" aria-haspopup="true" aria-expanded="false">
                         New <span class="caret"></span>
                     </a>
-                    <div class="dropdown-menu">
+                    <div class="dropdown-menu dropdown-menu-right">
                         <a class="dropdown-item" href="{{ route('sub.create') }}">Sub</a>
                         <a class="dropdown-item" href="#">Post</a>
                     </div>
@@ -20,7 +20,7 @@
                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" type="button" aria-haspopup="true" aria-expanded="false">
                     User <span class="caret"></span>
                 </a>
-                <div class="dropdown-menu">
+                <div class="dropdown-menu dropdown-menu-right">
                     @if(Auth::check())
                         <a class="dropdown-item" href="{{ route('user.show', Auth::user()->username) }}">@lang('general.profile')</a>
                         <a class="dropdown-item" href="{{ route('auth.logout') }}">@lang('general.logout')</a>
