a9774092e8cbbf33661b46b585cc212704d2941e
Nat Zimmermann <nat@natzim.me>
2015-09-13 14:11:41 +0100

    Arrange posts differently and remove thumbails

--- resources/views/partials/post.blade.php
+++ resources/views/partials/post.blade.php
@@ -1,37 +1,40 @@
-<div class="card card-block" data-sub="{{ $post->sub->name }}" data-slug="{{ $post->slug }}">
-    @if(!empty($post->thumbnail_url))
-        <img class="img-thumbnail pull-left" width="100" src="{{ asset($post->thumbnail_url) }}">
-    @endif
-    <h4 class="card-title">
-        <a href="{{ Helper::isValidUrl($post->content) ? $post->content : route('sub.post.show', [$post->sub->name, $post->slug]) }}">
-            {{ $post->title }}
+<div class="card" data-sub="{{ $post->sub->name }}" data-slug="{{ $post->slug }}">
+    <div class="card-block">
+
+        <h4 class="card-title">
+            <a href="{{ Helper::isValidUrl($post->content) ? $post->content : route('sub.post.show', [$post->sub->name, $post->slug]) }}">
+                {{ $post->title }}
+            </a>
+        </h4>
+        <h6 class="card-subtitle">
+            @choice('general.count.points', $score)
+            <span class="text-muted">{{ $post->created_at->diffForHumans() }}</span>
+        </h6>
+        @if(!empty($embedHtml))
+            </div>
+                {!! $embedHtml !!}
+            <div class="card-block">
+        @endif
+        @unless(Helper::isValidUrl($post->content))
+            <div class="card-text">
+                {!! Helper::markdownToHtml($post->content) !!}
+            </div>
+        @endunless
+        <a class="card-link" href="{{ route('sub.post.show', [$post->sub->name, $post->slug]) }}">
+            <i class="fa fa-comments"></i>
+            @choice('general.count.comments', $post->comments()->count())
         </a>
-    </h4>
-    <h6 class="card-subtitle">
-        @choice('general.count.points', $score)
-        <span class="text-muted">{{ $post->created_at->diffForHumans() }}</span>
-    </h6>
-    <div class="card-text">
-        @if(Helper::isValidUrl($post->content))
-            {!! $embedHtml !!}
-        @else
-            {!! Helper::markdownToHtml($post->content) !!}
+        <a class="card-link" href="{{ route('sub.show', $post->sub->name) }}">
+            <i class="fa fa-tag"></i>
+            {{ $post->sub->name }}
+        </a>
+        <a class="card-link" href="{{ route('user.show', $post->user->username) }}">
+            <i class="fa fa-user"></i>
+            {{ $post->user->username }}
+        </a>
+        @if(Auth::check())
+            <hr>
+            @include('partials.vote-buttons', compact('voteValue'))
         @endif
     </div>
-    <a class="card-link" href="{{ route('sub.post.show', [$post->sub->name, $post->slug]) }}">
-        <i class="fa fa-comments"></i>
-        @choice('general.count.comments', $post->comments()->count())
-    </a>
-    <a class="card-link" href="{{ route('sub.show', $post->sub->name) }}">
-        <i class="fa fa-tag"></i>
-        {{ $post->sub->name }}
-    </a>
-    <a class="card-link" href="{{ route('user.show', $post->user->username) }}">
-        <i class="fa fa-user"></i>
-        {{ $post->user->username }}
-    </a>
-    @if(Auth::check())
-        <hr>
-        @include('partials.vote-buttons', compact('voteValue'))
-    @endif
 </div>
--- resources/views/partials/posts.blade.php
+++ resources/views/partials/posts.blade.php
@@ -1,2 +1,10 @@
-@each('partials.post', $posts, 'post', 'partials.nothing')
-{!! $posts->render() !!}
+@if($posts->isEmpty())
+    @include('partials.nothing')
+@else
+    <div class="card-columns">
+        @foreach($posts as $post)
+            @include('partials.post', compact('post'))
+        @endforeach
+    </div>
+    {!! $posts->render() !!}
+@endif
