2ad93ab0f84009e9cdc947523ed9d3c47e6ccf91
Nat Zimmermann <nat@natzim.me>
2015-09-13 17:52:19 +0100

    Reformat gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,15 @@ var assetsDir = './resources/assets/';
 
 elixir(function (mix) {
     mix
-        .styles(
-        [
+        .styles([
             'bower/bootstrap/dist/css/bootstrap.css',
             'bower/font-awesome/css/font-awesome.css',
             'css/main.css'
-        ],
-        'public/css/style.css',
-        assetsDir
-    )
-        .scripts(
-        [
+        ], 'public/css/style.css', assetsDir)
+        .scripts([
             'bower/jquery/dist/jquery.js',
             'bower/bootstrap/dist/js/bootstrap.js',
             'js/main.js'
-        ],
-        'public/js/app.js',
-        assetsDir
-    )
-        .copy(
-        assetsDir + 'bower/font-awesome/fonts',
-        'public/fonts'
-    );
+        ], 'public/js/app.js', assetsDir)
+        .copy(assetsDir + 'bower/font-awesome/fonts', 'public/fonts');
 });
