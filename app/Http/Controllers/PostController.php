c05e567361a605101a41b5b792961b310859e239
Nat Zimmermann <nat@natzim.me>
2015-09-16 20:03:11 +0100

    Fix code style

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,6 @@ use App\Vote;
 
 class PostController extends Controller
 {
-
     public function __construct()
     {
         $this->middleware('auth', ['except' => ['show']]);
--- app/Laravit/SortableInterface.php
+++ app/Laravit/SortableInterface.php
@@ -5,7 +5,10 @@ namespace App\Laravit;
 interface SortableInterface
 {
     public function scopeHot($query);
+
     public function scopeNew($query);
+
     public function scopeOld($query);
+
     public function scopeTop($query);
 }
