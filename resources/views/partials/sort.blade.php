3c8a49d1cfd92e3e8a9aa592e7d4921afda0a974
Nat Zimmermann <nat@natzim.me>
2015-09-16 17:21:19 +0100

    Add frontend for post sorting

---
+++ resources/views/partials/sort.blade.php
@@ -0,0 +1,14 @@
+<div class="form-group">
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="sort-dropdown" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+            Sort
+        </button>
+        <div class="dropdown-menu" aria-labelledby="sort-dropdown">
+            <a class="dropdown-item" href="?sort=hot">Hot</a>
+            <a class="dropdown-item" href="?sort=new">New</a>
+            <a class="dropdown-item" href="?sort=old">Old</a>
+            <a class="dropdown-item" href="?sort=top">Top</a>
+        </div>
+    </div>
+</div>
--- resources/views/post/partials/posts.blade.php
+++ resources/views/post/partials/posts.blade.php
@@ -1,6 +1,7 @@
 @if($posts->isEmpty())
     @include('partials.nothing')
 @else
+    @include('partials.sort')
     <div class="card-columns">
         @foreach($posts as $post)
             @include('post.partials.post', compact('post'))
