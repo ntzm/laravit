1b57ffe1f35ba3bd30adc79c5077f2b15bf144ce
Nat Zimmermann <nat@natzim.me>
2015-09-16 17:16:20 +0100

    Add a bunch of new sorting options

--- app/Laravit/SortableInterface.php
+++ app/Laravit/SortableInterface.php
@@ -5,4 +5,7 @@ namespace App\Laravit;
 interface SortableInterface
 {
     public function scopeHot($query);
+    public function scopeNew($query);
+    public function scopeOld($query);
+    public function scopeTop($query);
 }
--- app/Laravit/SortableTrait.php
+++ app/Laravit/SortableTrait.php
@@ -7,7 +7,7 @@ use DB;
 trait SortableTrait
 {
     /**
-     * Hot resources.
+     * Query hot resources.
      *
      * @param \Illuminate\Database\Eloquent\Builder $query
      * @return \Illuminate\Database\Eloquent\Builder
@@ -18,5 +18,38 @@ trait SortableTrait
         return $query->orderBy(DB::raw(
             'log10(abs(score) + 1 ) * sign(score) + (unix_timestamp(created_at) / 300000)'
         ), 'desc');
+    }
+
+    /**
+     * Query new resources.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeNew($query)
+    {
+        return $query->orderBy('created_at', 'desc');
+    }
+
+    /**
+     * Query old resources.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeOld($query)
+    {
+        return $query->orderBy('created_at', 'asc');
+    }
+
+    /**
+     * Query top resources.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeTop($query)
+    {
+        return $query->orderBy('score', 'desc');
     }
 }
