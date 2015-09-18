af1d37cad0ee21a2fd2ce059e20bb844e7201b40
Nat Zimmermann <nat@natzim.me>
2015-09-18 10:57:12 +0100

    Refactor preview generator

--- app/Jobs/GeneratePreview.php
+++ app/Jobs/GeneratePreview.php
@@ -15,43 +15,83 @@ class GeneratePreview extends Job implements SelfHandling, ShouldQueue
 {
     use InteractsWithQueue, SerializesModels;
 
-    const PATH = 'images/previews/';
-    const FORMAT = 'jpg';
-
     private $post;
-
-    private function getPreviewPath($publicId)
-    {
-        return self::PATH.$publicId.'.'.self::FORMAT;
-    }
 
     public function __construct(Post $post)
     {
         $this->post = $post;
     }
 
+    /**
+     * Convert an image into a small blurred preview to be used on posts.
+     */
     public function handle()
     {
+        // This is called on every new post, so we will not be able to make a
+        // preview if the URL is unreadable.
         try {
-            $thumbnail = Image::make($this->post->content);
+            $image = Image::make($this->post->content);
         } catch (NotReadableException $e) {
             return;
         }
 
-        // Fit thumbnail to 16:9 ratio
-        $thumbnail->fit(320, 180);
-        // Blur thumbnail a little, because we will be scaling it up
-        $thumbnail->blur(5);
+        $preview = $this->makePreview($image);
+        $publicId = $this->makePublicId();
+        $this->store($preview, $this->getPath($publicId));
 
+        $this->post->thumbnail_url = $this->getPath($publicId);
+        $this->post->save();
+    }
+
+    /**
+     * Get the path where previews are stored.
+     *
+     * @param  string $publicId
+     * @return string
+     */
+    private function getPath($publicId)
+    {
+        return 'images/previews/'.$publicId.'.jpg';
+    }
+
+    /**
+     * Make image into preview.
+     *
+     * @param  \Intervention\Image\Image $image
+     * @return \Intervention\Image\Image
+     */
+    private function makePreview($image)
+    {
+        $image->fit(320, 180);
+        $image->blur(5);
+
+        return $image;
+    }
+
+    /**
+     * Make a unique public ID for storing the preview in storage.
+     *
+     * @return string
+     */
+    private function makePublicId()
+    {
         $publicId = uniqid();
-        // Just because uniqid isn't actually unique
-        while (Storage::exists($this->getPreviewPath($publicId))) {
-            $publicId = uniqid();
+
+        if (Storage::exists($this->getPath($publicId))) {
+            $this->makePublicId();
         }
 
-        Storage::put($this->getPreviewPath($publicId), $thumbnail->encode(self::FORMAT));
+        return $publicId;
+    }
 
-        $this->post->thumbnail_url = $this->getPreviewPath($publicId);
-        $this->post->save();
+    /**
+     * Store preview in storage.
+     *
+     * @param \Intervention\Image\Image $image
+     * @param string                    $path
+     */
+    private function store($image, $path)
+    {
+        Storage::put($path, $image->encode('jpg'));
     }
 }
